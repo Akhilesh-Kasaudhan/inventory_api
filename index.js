4930162a2f6c379ce3b312adb0afd79d1eb39b2b
Akhilesh-Kasaudhan <akhil2003kd@gmail.com>
2025-02-25 22:59:53 +0530

    cors error

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 const CLIENT_URL = process.env.CLIENT_URL;
 
 const corsOptions = {
-  origin: [CLIENT_URL, "https://ram.webexbytes.com/"],
+  origin: "*",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
